b7f12c4035b7b96539421e3896621e44166e745b
samarth.arun.narula <samarth.arun.narula@accenture.com>
2019-05-23 16:46:28 +0530

    drools

---
+++ src/main/java/com/acc/drool/controller/JewelleryShopController.java
@@ -0,0 +1,31 @@
+package com.acc.drool.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acc.drool.model.Product;
+import com.acc.drool.service.JewelleryShopService;
+
+@RestController
+public class JewelleryShopController {
+
+	private final JewelleryShopService jewelleryShopService;
+
+	@Autowired
+	public JewelleryShopController(JewelleryShopService jewelleryShopService) {
+		this.jewelleryShopService = jewelleryShopService;
+	}
+
+	@RequestMapping(value = "/getDiscount", method = RequestMethod.GET, produces = "application/json")
+	public Product getProductDiscount(
+			@RequestParam(required = true) String type) {
+
+		Product product = new Product();
+		product.setType(type);
+		return jewelleryShopService.getProductDiscount(product);
+	}
+
+}
---
+++ src/main/java/com/acc/drool/model/Product.java
@@ -0,0 +1,28 @@
+package com.acc.drool.model;
+
+public class Product {
+
+	private String type;
+	private int discount;
+	
+	public Product() {
+		
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
+}
